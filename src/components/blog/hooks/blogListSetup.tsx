69a9e5be70cf93583dccc4de4ffda2061ebf26f7
cameron <cameron_b@hotmail.co.uk>
2023-03-25 10:43:46 +0000

    Convert categories and view blog to static

--- src/components/blog/hooks/blogListSetup.tsx
+++ src/components/blog/hooks/blogListSetup.tsx
@@ -1,77 +1,48 @@
-import { useEffect, useMemo, useState } from "react";
-//import { useParams, useNavigate } from "react-router";
+import { useMemo, useState } from "react";
 import { useRouter } from "next/router";
-import useFetchBlogs, { FetchBlogs } from "../../../hooks/getBlogs";
-import useCategories from "../../../hooks/getCategories";
-import { Blog } from "../types";
+import { Category } from "../categories/types";
 
+interface Props {
+    categories: Category[];
+}
 
-export default function useBlogListSetup() {
+export default function useBlogListSetup({ categories }: Props) {
 
     const [category, setCategory] = useState<string>();
-    const [blogs, setBlogs] = useState<Blog[]>();
-    const [error, setError] = useState<string>();
 
-	//const params = useParams();
     const router = useRouter();
 
-    const { state, isLoading } = useCategories()
-    const { getBlogsPublished } = useFetchBlogs();
-
-    // returns undefined if data hasn't finished loading
     // Return boolean once categories are loaded and state is set
-    const categoryIsValid = useMemo((): undefined | boolean => {
-        if (isLoading && !category) return;
-        // Return true if category is all or matches any from the database
-        return (category === 'all' || state?.data.some(r => r.name === category));
+    function categoryIsValid() {
+        return category === 'all' || categories.some(r => r.name === category)
+    }
 
-    }, [category, state, isLoading]);
-
-    // Set the category when params change
-    useEffect(() => {
-        setCategory(String(router.query.id || ''));
-    }, [router.query]);
+    (function setCategoryState() {
+        const id = String(router.query.id || '');
+        if (id === category) return;
+        setCategory(id)
+    })();
 
     (function() {
+        if (!category) return;
         // When category is false redirect to all
         // Can be undefined so specific check is required
-        if (categoryIsValid === false) {
+        if (categoryIsValid() === false) {
             router.push('/blogs/categories/all');
             return;
         }
     })();
 
-    useEffect(() => {
-        if (!categoryIsValid) return;
-
-        (async () => {
-            const data = await getBlogsPublished({ published: true, category })
-
-            if (data.status === 200) {
-                setBlogs(data.data);
-                return;
-            }
-
-            setError('Error fetching blogs, please refresh the page and try again.');
-        })();
-
-    }, [category, categoryIsValid, getBlogsPublished]);
 
     const categoryLabel = useMemo(() => {
         if (typeof category !== 'string') return;
         return category[0].toUpperCase() + category.slice(1)
     }, [category]);
 
-    const isLoadingBlogs = useMemo((): boolean => {
-        if (blogs || error) return false;
-        return true;
-    }, [blogs, error]);
+
 
     return {
-        blogs,
-        isLoadingBlogs,
         category: categoryLabel,
-        error
     }
 
 }
