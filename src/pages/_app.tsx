5698470d770c242f12ee1255f68a1c45485660f1
cameron <cameron_b@hotmail.co.uk>
2023-03-26 12:09:37 +0100

    WIP: next auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,24 @@
 import Layout from '@/components/layout';
 import { AuthProvider } from '@/contexts/authContext';
+import initAuth from '@/initAuth';
 import '@/styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { initializeApp } from 'firebase/app';
 import type { AppProps } from 'next/app';
+initializeApp({
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.FIREBASE_APPID,
+})
+initAuth()
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<AuthProvider>
 			<Layout>
 				<Component {...pageProps} />
 			</Layout>
-		</AuthProvider>
 	)
 }
